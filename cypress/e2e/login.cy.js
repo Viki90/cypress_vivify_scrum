8d8250759f6fd5a398b65b6bb1db7a1cd045926b
Viki <viki.verebes90@gmail.com>
2022-12-14 18:21:04 +0100

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("login test", () => {
+    before("visit vivify scrum", () => {
+       
+    });
+
+    it("login with valid credentials", () => {
+       
+    });
+
+});
